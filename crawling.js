512dc5e0b943baa11baa275da91c8f96f60e4520
Seoyeonii <jsy2015171@gmail.com>
2024-01-28 00:34:56 +0900

    Refactor crawling.js to improve code readability and performance

--- crawling.js
+++ crawling.js
@@ -1,16 +1,49 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable consistent-return */
 /* eslint-disable no-underscore-dangle */
 // eslint-disable-next-line import/no-import-module-exports
 import puppeteer from 'puppeteer';
 
+const getProductInfosFromCurPage = async (page, curPage) => {
+  if (curPage < 1) return [];
+
+  await page.waitForSelector('.col-md-6');
+  const productInfos = await page.evaluate(() => {
+    const productInfosArr = Array.from(document.querySelectorAll('.col-md-6'));
+    return productInfosArr.map((el) => {
+      const [topDiv, bottomDiv] = el.children[0].children[0].children;
+      const [shopTypeEl, productTypeEl] = topDiv.children;
+      const shopType = shopTypeEl.innerText;
+      const productType = productTypeEl.innerText;
+      const [imgEl, otherEl] = bottomDiv.children;
+      const imgUrl = imgEl.children[0].src;
+      const productName = otherEl.querySelector('strong').innerHTML;
+      const m = otherEl.innerHTML.toString().match(/(\d+,\d+\s원)/);
+      let priceStr = 0;
+      if (m) priceStr = m[0].trim();
+      const price = parseInt(priceStr.toString().replace(/[^\d]/g, ''), 10);
+      return {
+        shopType,
+        productType,
+        imgUrl,
+        productName,
+        price,
+      };
+    });
+  });
+
+  // 다음 페이지로 넘어가기
+  await page.goto(
+    `https://pyony.com/search/?page=${curPage - 1}&event_type=&category=&item=100&sort=&price=&q=`,
+  );
+  const nextProductInfos = await getProductInfosFromCurPage(page, curPage - 1);
+  return [...productInfos, ...nextProductInfos];
+};
+
 const getProductInfos = async () => {
   try {
     const browser = await puppeteer.launch({
-      headless: false,
-      args: [
-        '--disable-web-security',
-        '--disable-features=IsolateOrigins',
-        ' --disable-site-isolation-trials',
-      ],
+      headless: true,
     });
     const page = await browser.newPage();
     await page.goto(
@@ -29,11 +62,13 @@ const getProductInfos = async () => {
     // 현재 페이지 location가져오면 페이지 번호 알 수 있음
     const lastPage = page.url().split('?')[1].split('&')[0].split('=')[1];
 
-    // 그 수만큼 반복하면 될듯?
-
-    // 정보 담아서 return
+    // 그 수만큼 반복
+    const productInfos = await getProductInfosFromCurPage(page, lastPage);
 
     await browser.close();
+
+    // 정보 담아서 return
+    return productInfos;
   } catch (error) {
     console.error(error);
   }
@@ -42,4 +77,3 @@ const getProductInfos = async () => {
 getProductInfos();
 
 // json 형태로 저장
-// 저장한 파일 백에게 전달
