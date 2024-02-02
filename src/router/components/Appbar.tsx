c325413cc6d2cace280da27a0b16c9a15f66ab00
Seoyeonii <jsy2015171@gmail.com>
2024-02-02 10:13:13 +0900

    :lipstick: [#4] 버튼 삭제

--- src/router/components/Appbar.tsx
+++ src/router/components/Appbar.tsx
@@ -1,7 +1,16 @@
-import { useMemo } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useCallback, useMemo } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import styled from 'styled-components';
+
+import {
+  ArrowLeftIcon,
+  BookmarkIcon,
+  LogoTitleIcon,
+  ShareIcon,
+} from '@/assets';
+
+import PATH from '../PATH';
 
 const StyledAppbar = styled.section`
   width: 100%;
@@ -12,24 +21,70 @@ const StyledAppbar = styled.section`
   padding: 0 20px;
   border-bottom: 1px solid #f6f6f6;
 
+  .item {
+    display: inline-flex;
+  }
+
+  svg {
+    border-radius: 0;
+    &:hover {
+      box-shadow: none;
+      cursor: default;
+    }
+  }
+
   .center {
     width: 100%;
     text-align: center;
+  }
+
+  .left {
+    display: inline-flex;
+  }
+  .right {
+    display: inline-flex;
+
+    .button-container {
+      display: inline-flex;
+      gap: 16px;
+    }
   }
 `;
 
 const Appbar = () => {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const handleClickGoBack = useCallback(() => {
+    navigate(-1);
+  }, [navigate]);
 
   const Content = useMemo(() => {
     switch (pathname) {
-      case '/profile':
+      case PATH.PROFILE:
         return <div className="center title01">마이페이지</div>;
-      case '/':
+      case PATH.CONTENT:
+        return (
+          <>
+            <div className="left" onClick={handleClickGoBack}>
+              <ArrowLeftIcon />
+            </div>
+            <div className="right">
+              <div className="button-container">
+                <ShareIcon />
+                <BookmarkIcon />
+              </div>
+            </div>
+          </>
+        );
+      case PATH.HOME:
       default:
-        return <div className="left title01">로고</div>;
+        return (
+          <div className="left title01">
+            <LogoTitleIcon />
+          </div>
+        );
     }
-  }, [pathname]);
+  }, [handleClickGoBack, pathname]);
 
   return <StyledAppbar>{Content}</StyledAppbar>;
 };
