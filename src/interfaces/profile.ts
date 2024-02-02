e352c330a98864a26083826ecf5865aa80887963
Seoyeonii <jsy2015171@gmail.com>
2024-02-02 16:14:11 +0900

    :sparkles: [#4] profile용 인터페이스 추가

--- src/interfaces/profile.ts
+++ src/interfaces/profile.ts
@@ -1,6 +1,12 @@
+import { Product } from './common';
+
 export interface ProfileInfo {
   userId: number;
   nickname: string;
   email: string;
   profileImageUrl: string;
+}
+
+export interface SaveItemInfo extends Product {
+  categoryName: string;
 }
